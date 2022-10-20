745250f317da87d547d03fe5fc8a5aa1d81083e4
williamdubgod <william_vulcano@hotmail.com>
2022-10-20 16:27:27 -0300

    curso JS avançado finalizado

--- javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,15 +6,40 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
+        this._listaNegociacoes = new ListaNegociacoes();
 
-    }
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+    }   
 
     adiciona(event) {
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
-
+        this._mensagem.texto = 'Negociação adicionada com sucesso.'
+        this._mensagemView.update(this._mensagem);
         
-        console.log(negociacao);
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
